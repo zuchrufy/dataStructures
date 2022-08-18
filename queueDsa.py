4d1c83da24b89de904628b2a7a2e5f2b32de7732
adrianrufy <zuhcrufy.25@gmail.com>
2022-08-18 08:27:19 +0700

    ini adalah queue

---
+++ queueDsa.py
@@ -0,0 +1,32 @@
+# queue data structure adalah sebuah data structure yang memungkinkan sebuah data berjalan seperti antrian
+
+
+# prinsip queue adalah first in first out (FIFO) yang mana berarti;
+
+#         bahwa setiap proses akan menyelesaikan data y ang pertama kali masuk dan mengeluarkan inputan data yang pertama kali masuk 
+
+
+# contoh pseudocode dari queue
+
+#         1. bentuk sebuah array data [1,2,3,4,5]
+#         2. maka yang akan dikeluarkan pertama kali adalah index array[0]
+        
+
+
+#   basic operation dari queue :
+
+basic_operation = {
+
+    "enqueue" : "menambahkan element pada akhir dari queue",
+
+    "dequeue" : "menghilangkan atau mengeluarkan item dari depan queue",
+
+    "isEmpty" : "memeriksa apakah queueu kosong atau tidak",
+
+    "isFull"  : "untuk mengecek apakah queue penuh atau tidak",
+
+    "peek"    : "mengambil value didepan queue tanpa menghilangkan indexnya"
+
+}
+
+print(basic_operation)
